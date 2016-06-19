738eb8d621412f2e4cd10a795a08490300434980
Mike Arvela <mike.arvela@futurice.com>
2016-06-19 12:08:39 +0300

    Add initial 'compile' script

---
+++ app/cake.ts
@@ -0,0 +1,10 @@
+class Greeter {
+    constructor(public greeting: string) { }
+    greet() {
+        return "<h1>" + this.greeting + "</h1>";
+    }
+};
+
+var greeter = new Greeter("Cake, oh my!");
+    
+document.body.innerHTML = greeter.greet();
